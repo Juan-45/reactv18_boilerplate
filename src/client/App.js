0a458223bfe3500eaf7fb1508ddd6aa7ef115bee
Juan-45 <quijuan12@gmail.com>
2022-12-08 21:22:18 -0300

    Add basic examples

--- src/client/App.js
+++ src/client/App.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { Grid, Typography } from "@mui/material";
+import theme from "./theme/theme";
 
 const App = () => {
-  return <div>Welcome to React Boilerplate App</div>;
+  return (
+    <Grid container direction='column' alignItems='center'>
+      <Grid container sx={{ background: theme.palette.primary.light }}>
+        <Typography color={theme.palette.primary.contrastText}>
+          Test: background custom primary - light, texto color primary
+          contrastText
+        </Typography>
+      </Grid>
+      <Grid container sx={{ background: theme.palette.primary.main }}>
+        <Typography color={theme.palette.primary.contrastText}>
+          Test: background custom primary - main, texto color primary
+          contrastText
+        </Typography>
+      </Grid>
+      <Grid container sx={{ background: theme.palette.primary.dark }}>
+        <Typography color={theme.palette.primary.contrastText}>
+          Test: background custom primary - dark, texto color primary
+          contrastText
+        </Typography>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default App;
