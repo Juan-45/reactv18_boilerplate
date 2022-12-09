2fc0c175d8a878a1fa453e77ac2709b337e4dd47
Juan-45 <quijuan12@gmail.com>
2022-12-08 21:21:19 -0300

    Add ThemeProvider and CssBaseline

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import theme from "./theme/theme";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
@@ -8,6 +10,9 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline enableColorScheme />
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
