2e069eeb23918ba14dfc69161b9976356d6782e3
Juan-45 <quijuan12@gmail.com>
2022-12-08 21:21:52 -0300

    Add theme with basic palette sample

---
+++ src/client/theme/theme.js
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#004d40",
+    },
+    tonalOffset: "0.3",
+  },
+});
+
+export default theme;
